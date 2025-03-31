fcfa75f89e6093da87e700b98aa176fbe9db49e3
alejandrochmejia <alejandrochmejia@gmail.com>
2025-03-31 09:14:07 -0400

    Add new additional items, beverages, salads, and desserts to the menu

--- Pizzeria/data/map.js
+++ Pizzeria/data/map.js
@@ -2,11 +2,46 @@ import margarita from '../assets/menu/pizzas/margarita.png';
 import pepperoni from '../assets/menu/pizzas/pepperoni.png';
 import pollo from '../assets/menu/pizzas/pollo.png';
 import vegetariana from '../assets/menu/pizzas/vegetariana.png';
-// ... otras imágenes
+
+import papasfritas from '../assets/menu/adicionales/papas-fritas.png';
+import tequenos from '../assets/menu/adicionales/tequeños.png';
+
+import sevenup from '../assets/menu/bebidas/7up.png';
+import cocacola from '../assets/menu/bebidas/cocacola.png';
+import fantanaranja from '../assets/menu/bebidas/fanta-naranja.png';
+import fantauva from '../assets/menu/bebidas/fanta-uva.png';
+import goldenkolita from '../assets/menu/bebidas/golden-kolita.png';
+import pepsi from '../assets/menu/bebidas/pepsi.png';
+
+import ensaladacesar from '../assets/menu/ensaladas/ensalada-cesar.png';
+
+import cheesecakefresa from '../assets/menu/postres/cheesecake-fresa.png';
 
 export const pizzaImages = {
     margarita: margarita,
     pepperoni: pepperoni,
     pollo: pollo,
     vegetariana: vegetariana,
+};
+
+export const adicionalesImages = {
+    papasfritas: papasfritas,
+    tequenos: tequenos,
+};
+
+export const bebidaImages = {
+    sevenup: sevenup,
+    cocacola: cocacola,
+    fantanaranja: fantanaranja,
+    fantauva: fantauva,
+    goldenkolita: goldenkolita,
+    pepsi: pepsi,
+};
+
+export const ensaladaImages = {
+    ensaladacesar: ensaladacesar,
+};
+
+export const postreImages = {
+    cheesecakefresa: cheesecakefresa,
 };
